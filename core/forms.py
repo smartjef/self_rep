55e929736156a262560e7370a016f43fafe35bbf
JEFF ODHIAMBO <jeff.odhiambo@studentambassadors.com>
2023-02-26 13:44:05 +0300

    done

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
-# class AdvocateSignUpForm(forms.Form):
-#     username =
-#     pass
+class AdvocateSignUpForm(UserCreationForm):
+    field = forms.CharField(max_length=255, required=True)
+    email = forms.EmailField(max_length=255, required=True)
+    username = forms.CharField(max_length=50, required=True)
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2', 'field')
